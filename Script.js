ee78fce8d69c1065c763f70fc5a5f6a9e4e32760
Fanny Johansson <fannyjohansson@Fannys-MBP.lan>
2020-11-05 14:47:08 +0100

    Redid the structure, css variables, responsiveness, and toggle menu
    Converted the last structure to grid in most places, changed my colors in css to vaiables instead of seperate ones for easy change. Created a toggle menu for the mobil version. Also implemented some responsivness that needs more work but works ok on homescreen.

---
+++ Script.js
@@ -0,0 +1,10 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+
+function toggleNavBar(){
+    mainNav.classList.toggle('active');
+}
+
+
+navBarToggle.addEventListener('click', toggleNavBar) ; 
